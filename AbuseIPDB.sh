51d918b49082d28a60a36eadc06ee7e1788836ac
csduncan06 <96197413+csduncan06@users.noreply.github.com>
2022-05-25 17:05:41 +0100

    Rewrite!
    + Improves user experience (easier to use)
    + Made logging easier (easy to use awk in output.txt)
    + Fixed a million bugs

--- AbuseIPDB.sh
+++ AbuseIPDB.sh
@@ -1,17 +1,15 @@
-API_KEY="AbuseIPDB-API-KEY"
+API_KEY="APIKey"
 
-clear
-if [ "$1" == "" ]
-then
-echo "[!] Please enter a filepath that contains IP-Addresses"
-fi
+ip_list=( $( cat /var/log/httpd/access_log | awk '{print $1}' | uniq | sort -n | sort -nr ) ) #Get all Apache IP addresses and organise
 
-echo "| AbuseIPDB quickscan at $(date)" > output.txt
+echo
+echo "Format:"
+echo "IP, Abuse confidence, Country"
+echo
 
-ip_list=( $(cat $1 | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -u) )
-pot_mal_ip_count=0
-
+{
 for ip in "${ip_list[@]}"; do
+
 json_req_data=$(curl -G https://api.abuseipdb.com/api/v2/check \
         --data-urlencode """ipAddress=$ip""" \
         -d verbose \
@@ -19,26 +17,15 @@ json_req_data=$(curl -G https://api.abuseipdb.com/api/v2/check \
         -H "Accept: application/json" 2>/dev/null | jq .)
 
 abuseConfidenceScore=$(echo $json_req_data | jq -r '.data.abuseConfidenceScore')
-usageType=$(echo $json_req_data | jq -r '.data.usageType')
 countryName=$(echo $json_req_data | jq -r '.data.countryName')
 
 if [ "$abuseConfidenceScore" -ge 1 ]; then
-pot_mal_ip_count=$((pot_mal_ip_count+1))
+echo "$ip $abuseConfidenceScore '$countryName'"
 fi
 
-
-echo "checking IP-Address: $ip
-Abuse confidence : $abuseConfidenceScore %
-Country: $countryName
-Usage: $usageType
-" >> output.txt
 done
+} | tee output.txt
 
-
-echo "Total number of IP-Addresses:"
-echo "$(wc -l $1 | awk '{ print $1 }')"
 echo
-echo "Total number of potentially malicious IP-Addresses:"
-echo $pot_mal_ip_count
-echo
-echo "Log can be found in 'output.txt' "
+echo "Total potentially malicious IP's : $( (cat output.txt | wc -l ) )"
+echo "Log file             : output.txt"
